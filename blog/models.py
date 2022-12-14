736a76fc597b0897f7be26214284383c2a9b35dc
Aspen92 <asplundchristofer@gmail.com>
2022-12-14 09:37:04 +0000

    Install summernote, import include method in urls.py and update urlspatterns to include summernote path. Add django_summernote to installed apps in settings.py. Create new class in admin.py called PostAdmin

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,9 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
-# Models
+# MODELS
+
+# Post Model
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -26,6 +28,7 @@ class Post(models.Model):
     def number_of_likes(self):
         return self.likes.count()
 
+# Comment Model
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
